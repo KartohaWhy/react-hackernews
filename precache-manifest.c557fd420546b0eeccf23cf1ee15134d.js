33292c747313e0b0b703229981dcf469832e5729
unknown <romka.fominyx@mail.ru>
2020-10-21 18:45:25 +0400

    Updates

---
+++ precache-manifest.c557fd420546b0eeccf23cf1ee15134d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983395d83b9dde069c3321a8a08d4dbb",
+    "url": "/react-hackernews/index.html"
+  },
+  {
+    "revision": "69a763cfc1ffe2af6d46",
+    "url": "/react-hackernews/static/css/main.bb4973b4.chunk.css"
+  },
+  {
+    "revision": "6d5a1b8b317d0e05efba",
+    "url": "/react-hackernews/static/js/2.94abb649.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/react-hackernews/static/js/2.94abb649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a763cfc1ffe2af6d46",
+    "url": "/react-hackernews/static/js/main.bd98cb97.chunk.js"
+  },
+  {
+    "revision": "a66ff8556d8ffefaa025",
+    "url": "/react-hackernews/static/js/runtime-main.3f07065d.js"
+  },
+  {
+    "revision": "bf5a18f862ac6cd7e774f8de858e4542",
+    "url": "/react-hackernews/static/media/logo.bf5a18f8.svg"
+  }
+]);
